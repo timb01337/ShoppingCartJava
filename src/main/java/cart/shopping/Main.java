45c861d13d48c25b0add0fb8562ea73dea0a4079
Tim Kastl <Kastl123@gmx.de>
2025-04-10 15:43:42 +0200

    added args for console only mode

--- src/main/java/cart/shopping/Main.java
+++ src/main/java/cart/shopping/Main.java
@@ -1,8 +1,35 @@
 package cart.shopping;
 
+import cart.shopping.gui.MainWindow;
+
 public class Main {
+
+    private static final String APPLICATION_NAME = "Java Shopping Cart";
+    private static final String CONSOLE_ONLY_ARGUMENT = "--console-only";
+
     public static void main(String[] args) {
 
+        var consoleOnlyMode = false;
+
+        for (String arg : args) {
+            if (CONSOLE_ONLY_ARGUMENT.equals(arg)) {
+                consoleOnlyMode = true;
+                break;
+            }
+        }
+
+        if (consoleOnlyMode) {
+
+            System.out.println("Console application started.");
+
+
+        } else {
+            System.out.println("GUI application started.");
+
+            var mainWindow = new MainWindow(APPLICATION_NAME);
+            mainWindow.setVisible(true);
+
+        }
 
 
     }
---
+++ src/main/java/cart/shopping/gui/MainWindow.java
@@ -0,0 +1,16 @@
+package cart.shopping.gui;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+
+    public MainWindow(String applicationName) {
+
+        setTitle(applicationName);
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+
+    }
+}
